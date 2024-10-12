c73a4b7a4791f6100d9a6d42448efa76fd93afa8
Sajeeldev <sajeelwebdev@gmail.com>
2024-10-13 00:14:27 +0500

    AuthApi

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+// This resource function take the name and the resource controller that would create all the routes for us
+// apiresource is convenient function similar to resource function but this function is not looking to create and edit becuase that belongs to web application
+Route::resource('/',[PostController::class]);
 
 Route::get('/', function(){
     return 'API';
